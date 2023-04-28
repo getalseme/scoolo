fb4c59c62654717c08fdc83b737d9475e9a855cc
getalSemenz <giulio.semenzato@itiszuccante.edu.it>
2023-04-28 12:01:21 +0200

    adjusted some things

---
+++ front_end/e-shop_testing/puttanata/app.js
@@ -0,0 +1,26 @@
+
+Vue.createApp({
+    data(){
+            return {
+                activePage: 0,
+                pages: [
+                    {
+                        pageTitle: 'Home page',
+                        content: 'Wellcome to the wonderful world of underground music',
+                        link: {text: 'Home', url: 'index.html'}
+                    },
+                    {
+                        pageTitle: 'About page',
+                        content: 'bla bla bla bla bla',
+                        link: {text: 'About', url: 'about.html'}
+                    },
+                    {
+                        pageTitle: 'Contact page',
+                        content: 'GET THE FUCK OUT',
+                        link: {text: 'Contact', url: 'contact.html'}
+                    }
+                ]
+            };
+        }
+    }).mount('body');
+
