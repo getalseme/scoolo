cb11d4f5c4b408a419fa0b2d71a58218a4f1a763
getalSemenz <giulio.semenzato@itiszuccante.edu.it>
2023-05-04 21:30:33 +0200

    graphic is better

--- front_end/e-shop_testing/public/server.js
+++ front_end/e-shop_testing/public/server.js
@@ -1,30 +1,31 @@
-const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  if (req.url === '/clothing') {
-    res.setHeader('Content-Type', 'application/json');
-    fs.readFile('clothing.json', (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  }
-  else if(req.url === '/music'){
-    res.setHeader('Content-Type', 'application/json');
-    fs.readFile('music.json', (err, data) => {
-      if (err) throw err;
-      res.end(data);
-    });
-  }
-  // Return a 404 error for all other requests
-  else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.end('<h1>404 Not Found</h1>');
-  }
-});
+  const http = require('http');
+  const fs = require('fs');
 
-server.listen(8000, () => {
-  console.log('Server listening on port 8000');
-});
+  const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.url === '/clothing') {
+      res.setHeader('Content-Type', 'application/json');
+      fs.readFile('clothing.json', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+      });
+    }
+    else if(req.url === '/music'){
+      res.setHeader('Content-Type', 'application/json');
+      fs.readFile('music.json', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+      });
+    }
+    // Return a 404 error for all other requests
+    else {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.end('<h1>404 Not Found</h1>');
+    }
+  });
+
+  server.listen(8000, () => {
+    console.log('Server listening on port 8000');
+  });
