312f06fc88adfcc0ba553b9a2c34289f084e7d40
getalSemenz <giulio.semenzato@itiszuccante.edu.it>
2023-05-16 16:25:06 +0200

    robs

--- sitoSwag/index.js
+++ sitoSwag/index.js
@@ -2,7 +2,7 @@ const menuOn = false;
 
 function menu(){
     let menu = document.getElementById("menu");
-    if (menu.style.display === "none") {
+    if (menu.style.display === "none" || menu.style.display === "") {
         menu.style.display = "block";
     } else {
         menu.style.display = "none";
